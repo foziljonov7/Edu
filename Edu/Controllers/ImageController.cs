d7adbe3022e9a589915cd8a56d72bce750efa57a
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-09 14:28:00 +0500

    ...

---
+++ Edu/Controllers/ImageController.cs
@@ -0,0 +1,13 @@
+﻿using Edu.Entities;
+using Edu.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        
+    }
+}
