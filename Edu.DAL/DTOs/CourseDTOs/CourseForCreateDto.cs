f2a30f4aa11890960b3489fb5a1f64cb3cb719e1
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-04-23 11:16:35 +0500

    ...

---
+++ Edu.DAL/DTOs/CourseDTOs/CourseForCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using Edu.Domain.Models;
+using System.Collections.ObjectModel;
+
+namespace Edu.DAL.DTOs.CourseDTOs;
+
+public class CourseForCreateDto
+{
+    public int SubjectId { get; set; }
+    public int TeacherId { get; set; }
+    public DateTimeOffset StartingDate { get; set; }
+    public decimal Price { get; set; }
+    public Collection<Student> Students { get; set; }
+}
